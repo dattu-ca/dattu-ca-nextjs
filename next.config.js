b07dc4dba25264dc66e97d8c39eb5e091a609ba1
Dattu Patel <patel.alucard@gmail.com>
2022-01-06 07:16:38 -0500

    added material ui

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-module.exports = {
+const withTM = require("next-transpile-modules")([
+  "@mui/material",
+  "@mui/system",
+  "@mui/icons-material", // If @mui/icons-material is being used
+]);
+
+module.exports = withTM({
   images: {
-    loader: 'custom',
+    loader: "custom",
   },
-}
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    };
+    return config;
+  },
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/index.css'
+
+
+
+
+import React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { theme } from "../styles/theme";
+import { WEBSITE_NAME } from '../lib/constants'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Next.js Blog Example with {WEBSITE_NAME}</title>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;

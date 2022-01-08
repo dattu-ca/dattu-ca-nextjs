9d67ce4771affefab5fdd378c181d264c1dda903
Dattu Patel <patel.alucard@gmail.com>
2022-01-07 20:38:42 -0500

    Removed json view

--- components/posts/posts.blurb.js
+++ components/posts/posts.blurb.js
@@ -1,12 +1,10 @@
 
 import { Paper, Typography } from "@mui/material";
-import ReactJson from "react-json-view";
 
 const PostBlurb = ({ post }) => {
   return (
     <Paper component="article">
       <Typography component="h1" variant="h1">{post.fields.title}</Typography>
-      <ReactJson src={post}/>
     </Paper>
   );
 };

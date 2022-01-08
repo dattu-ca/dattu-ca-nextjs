ef6cb31ca575310266a6eeddab1f0c2b466f0cf3
Dattu Patel <patel.alucard@gmail.com>
2022-01-08 07:56:17 -0500

    Resetting only to show index

--- pages/tag/[slug]/[num].js
+++
@@ -1,5 +0,0 @@
-const PostsList = () => {
-    return <h1>HELLO WORLD</h1>
-}
-
-export default PostsList;
--- pages/tag/[slug]/index.js
+++
@@ -1,5 +0,0 @@
-const PostsList = () => {
-    return <h1>HELLO WORLD Page 1</h1>
-}
-
-export default PostsList;

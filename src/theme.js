5806ba1ab9ac07e2489efd4d58db85010d6701ab
Dattu Patel <patel.alucard@gmail.com>
2022-01-07 22:06:35 -0500

    Fixing

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,40 @@
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
+import { grey, orange } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
+    type: 'light',
+    background:{
+      default: grey[100],
+      paper: 'white'
+    },
     primary: {
-      main: '#556cd6',
+      main: '#3897c3',
+      contrastText: '#ffffff',
     },
     secondary: {
-      main: '#19857b',
+      main: '#FD6B03',
+      contrastText: '#ffffff',
+    },
+    warning: {
+      main: '#ff5252',
     },
     error: {
-      main: red.A400,
+      main: '#d50000',
     },
+    info: {
+      main: '#00b8d4',
+    },
+  },
+  props: {
+    MuiTooltip: {
+      arrow: true,
+    },
+  },
+  typography: {
+    
   },
 });
 
